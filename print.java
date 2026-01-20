081e6bc93d3d4f008e12fb0dfa7d54f89e4a4501
cursed2024 <ashtondsouza289@gmail.com>
2026-01-20 11:45:35 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,7 @@
+class print(){
+    public static void maain(Strings args[]){
+        System.out.print("Hello");
+        System.out.println(" World");
+        Systemm.out.print("\n This is my second code in java");
+    }
+}
