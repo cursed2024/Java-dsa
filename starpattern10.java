e989f2ac95e382ded39ac4066d82c837b689b599
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-23 13:31:50 +0530

    initial

---
+++ starpattern10.java
@@ -0,0 +1,20 @@
+class starpattern10{
+//1
+//23
+//456
+//78910
+//1112131415
+
+    public static void main(String args[]){
+        int n=5;
+        int number =1;
+        for(int i =1 ; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+
+    }
+}
