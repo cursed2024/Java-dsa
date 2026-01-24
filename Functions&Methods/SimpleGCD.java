beda92eda7165274c49125cc39f6371ab6ee5059
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-24 12:29:05 +0530

    initial

---
+++ Functions&Methods/SimpleGCD.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class SimpleGCD {
+
+    public static int findGCD(int a, int b) {
+        int gcd = 1;
+
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("GCD is: " + findGCD(num1, num2));
+
+        sc.close();
+    }
+}
