dbb2ca19b5189ccbe691c938e13f8ce25fd6e50f
Ashton dsouza <ashtondsouza289@gmail.com>
2026-01-25 13:19:25 +0530

    intial

---
+++ SimpleArray.java
@@ -0,0 +1,27 @@
+//output
+/*
+
+Marks :95
+Marks :45
+Marks :87
+Marks :85
+Marks :63
+
+*/
+
+class SimpleArray{
+    public static void main(String[] args) {
+        int[] Marks = new int[5];
+        Marks[0] = 95;
+        Marks[1] = 45;
+        Marks[2] = 87;
+        Marks[3] = 85;
+        Marks[4] = 63;
+
+        System.out.println("Marks :"+ Marks[0]);
+        System.out.println("Marks :"+ Marks[1]);
+        System.out.println("Marks :"+ Marks[2]);
+        System.out.println("Marks :"+ Marks[3]);
+        System.out.println("Marks :"+ Marks[4]);
+    }
+}
